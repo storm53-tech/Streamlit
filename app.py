2bdd8b6677443ea94ec5e0382228e51e3ffb49d9
ACLGraft-LindyScore <kc.herne@yahoo.com>
2024-08-07 14:09:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def fetch_latest_data():
     """
     try:
         # Public URL of the file
-        public_url = 'https://storage.googleapis.com/your-bucket-name/Files.zip'  # Update with your actual public file URL
+        public_url = 'https://storage.googleapis.com/lindyscore/Files.zip'  # Update with your actual public file URL
 
         # Download the zip file from the public URL
         response = requests.get(public_url)
@@ -98,4 +98,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
